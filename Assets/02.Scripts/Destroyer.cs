017d68ead0904248981db3e5e9fff6b82d889121
spirit790 <126559201+spirit790@users.noreply.github.com>
2023-03-16 16:18:48 +0900

    UI 변경
    라이프 감소 text & Scripts
    골드 증가 text & Scripts

--- Assets/02.Scripts/Destroyer.cs
+++ Assets/02.Scripts/Destroyer.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Destroyer : MonoBehaviour
 {
+    public int life = 20;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
+            life--;
             other.GetComponentInChildren<HUDHpBar>().DestroyHpBar();
             Destroy(other.gameObject);
         }
--- Assets/02.Scripts/UIManager.cs
+++ Assets/02.Scripts/UIManager.cs
@@ -5,18 +5,27 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public Text gold;
     public Text killCountText;
     public Text lvText;
+    public Text life;
+    public int hert = 20;
     public GameMgr gameMgr;
+    public Destroyer destroyer;
     void Start()
     {
+        life.text = "Life : "+ hert;
         gameMgr = GameObject.Find("GameMgr").GetComponent<GameMgr>();
+        destroyer = GameObject.Find("Destroyer").GetComponent<Destroyer>();
     }
 
     void Update()
     {
+        life.text = "Life : "+ destroyer.life;
+        gold.text = "Gold : " + gameMgr.killCnt * 100;
         killCountText.text = "KILL : "+gameMgr.killCnt;
         lvText.text = "LV" + gameMgr.curLV;
+
     }
 
     public void RangeOff()
